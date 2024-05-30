4f27513355fac721e4c7a2b93aed00056199b261
Darshan525 <mpdarshan525@gmail.com>
2024-05-30 12:39:31 +0530

    added html

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import About1 from './Compounts/About';
+
+function App() {
+  return (
+    <>
+    <About1></About1>
+    </>
+  );
+}
+
+export default App;
